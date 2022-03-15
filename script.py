52fb1cee8fb4a5414c7e250b963d89c19f86e1ea
Nicolas J. Dowdy <njdowdy@gmail.com>
2022-03-15 10:19:40 -0500

    Remove running as script for deployment

--- script.py
+++ script.py
@@ -1,15 +1,6 @@
 from catalog_suffix_fixer import process as p
 
-myfile = (
-    "catalog_suffix_fixer/Import Copy of History Catalog Data Entry H19235 to 22925.csv"
-)
 
-
-def main(path: str) -> dict:
+def process(path: str) -> dict:
     result = p.process_file(file=path)
     return result
-
-
-# if __name__ == "__main__":
-#     result = main(myfile)
-#     print(result)
