fd359402eb3f9844e6be37aaf66569fff5584747
Nicolas J. Dowdy <njdowdy@gmail.com>
2022-03-15 10:11:19 -0500

    first commit

---
+++ catalog_suffix_fixer/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.0"
+
+from catalog_suffix_fixer.functions import process as process
+from catalog_suffix_fixer.functions import parsing as parse
---
+++ catalog_suffix_fixer/functions/process.py
@@ -0,0 +1,23 @@
+import re
+from catalog_suffix_fixer.functions import parsing as parse
+
+
+def process_file(file: str) -> dict:
+    # do a thing
+    df = parse.import_data(file)
+    if not parse.verify_format(df):
+        return {"ERROR": "File not in required format."}
+    df = parse.sort_df(df)
+    df = parse.remove_empty_rows(df)
+    processed_count = parse.number_processed(df)
+    df = parse.generate_new_records(df)
+    generated_count = parse.number_generated(df, processed_count)
+    new_total_count = processed_count + generated_count
+    # check if file exists in output
+    file_out = re.sub(r"\..*$", "_processed.tsv", file)
+    parse.save_df(df, file_out)
+    return {
+        "records_in": processed_count,
+        "records_out": new_total_count,
+        "records_generated": generated_count,
+    }
